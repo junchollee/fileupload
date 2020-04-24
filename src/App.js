11b7aed139bd9b635cc657c6f9511f889b2c158e
이준철 <junchol.lee@newin.co.kr>
2020-04-24 17:02:22 +0900

    fir commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'; 
+const cors = require('cors');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component { 
+    
+    state = { 
+  
+      // Initially, no file is selected 
+      selectedFile: null
+    }; 
+     
+    // On file select (from the pop up) 
+    onFileChange = event => { 
+     
+      // Update the state 
+      this.setState({ selectedFile: event.target.files[0] }); 
+     
+    }; 
+     
+    // On file upload (click the upload button) 
+    onFileUpload = async () => { 
+      
+      const preSignedUrl = await getPreSignedUrl(this.state.selectedFile.name );
+      // Create an object of formData 
+      const formData = new FormData(); 
+     
+      // Update the formData object 
+      formData.append( 
+        "myFile", 
+        this.state.selectedFile, 
+        this.state.selectedFile.name 
+      ); 
+     
+      // Details of the uploaded file 
+      console.log(this.state.selectedFile); 
+     
+      // Request made to the backend api 
+      // Send formData object 
+      uploadToBucket(preSignedUrl, formData);
+    }; 
+     
+    // File content to be displayed after 
+    // file upload is complete 
+    fileData = () => { 
+     
+      if (this.state.selectedFile) { 
+          
+        return ( 
+          <div> 
+            <h2>File Details:</h2> 
+            <p>File Name: {this.state.selectedFile.name}</p> 
+            <p>File Type: {this.state.selectedFile.type}</p> 
+            <p> 
+              Last Modified:{" "} 
+              {this.state.selectedFile.lastModifiedDate.toDateString()} 
+            </p> 
+          </div> 
+        ); 
+      } else { 
+        return ( 
+          <div> 
+            <br /> 
+            <h4>Choose before Pressing the Upload button</h4> 
+          </div> 
+        ); 
+      } 
+    }; 
+     
+    render() { 
+     
+      return ( 
+        <div> 
+            <h1> 
+              GeeksforGeeks 
+            </h1> 
+            <h3> 
+              File Upload using React! 
+            </h3> 
+            <div> 
+                <input type="file" onChange={this.onFileChange} /> 
+                <button onClick={this.onFileUpload}> 
+                  Upload! 
+                </button> 
+            </div> 
+          {this.fileData()} 
+        </div> 
+      ); 
+    } 
+  } 
+  
+  const getPreSignedUrl = async fileName => {
+    const response = await fetch('https://cmuwvepfg6.execute-api.ap-northeast-2.amazonaws.com/prod/presignedurl', {mode: 'cors'}, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json;charset=utf-8"
+      },
+      body: JSON.stringify({ fileName })
+    });
+  
+    const url = await response.json();
+    return url;
+  };
 
-export default App;
+  const uploadToBucket = async (preSignedUrl, file) => {
+    const option = {
+      method: "PUT",
+      body: file
+    };
+  
+    await fetch(preSignedUrl, option);
+  };
+
+  export default App; 
